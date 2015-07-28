99f4729c68c8d075dfd1885ee2cf2e5f47654e5e
Sergey Tikhonov <zimbler@gmail.com>
2015-07-28 13:43:54 +0300

    fix seq, remove path from logg

--- logcollect/formatter.py
+++ logcollect/formatter.py
@@ -14,15 +14,17 @@ class AMQPLogstashFormatter(logging.Formatter):
     skip_list = (
         'args', 'asctime', 'created', 'exc_info', 'exc_text', 'filename',
         'funcName', 'id', 'levelname', 'levelno', 'lineno', 'module',
-        'msecs', 'msecs', 'message', 'msg', 'name', 'path',
+        'msecs', 'msecs', 'message', 'msg', 'name', 'pathname',
         'processName', 'relativeCreated', 'thread', 'threadName', 'extra')
 
     def __init__(self, message_type='Logstash', tags=None, fqdn=False,
-                 activity_identity={}):
+                 activity_identity={}, max_seq_no=1000):
         super(AMQPLogstashFormatter, self).__init__()
         self.message_type = message_type
         self.tags = tags if tags is not None else []
-        self.seq_no = 1
+        self._seq_no = 1
+        self._max_seq_no = max_seq_no
+        self.ts = None
 
         if fqdn:
             self.host = socket.getfqdn()
@@ -68,7 +70,16 @@ class AMQPLogstashFormatter(logging.Formatter):
         if sys.version_info < (3, 0):
             return json.dumps(message)
         else:
+            # noinspection PyArgumentList
             return bytes(json.dumps(message), 'utf-8')
+
+    def get_seq_no(self, created):
+        if created != self.ts:
+            self.ts = created
+            self._seq_no = 0
+        if self._seq_no < self._max_seq_no:
+            self._seq_no += 1
+        return self._seq_no
 
     def format(self, record):
         # Create message dict
@@ -77,15 +88,13 @@ class AMQPLogstashFormatter(logging.Formatter):
             '@version': 1,
             'message': record.getMessage(),
             'host': self.host,
-            'path': record.pathname,
             'tags': self.tags,
             'type': self.message_type,
-            'seq': self.seq_no,
+            'seq': self.get_seq_no(record.created),
             # Extra Fields
             'levelname': record.levelname,
             'logger': record.name,
         }
-        self.seq_no += 1
 
         # Add extra fields
         message.update(self.get_extra_fields(record))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='logcollect',
-    version='0.6',
+    version='0.7',
     packages=['logcollect'],
     url='http://github.com/rutube/logcollect/',
     license='Beer license',
