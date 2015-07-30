342316a4135edab7824c590e5527c947f2393eeb
Sergey Tikhonov <zimbler@gmail.com>
2015-07-30 13:57:33 +0300

    create rst for pypo

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 setup(
     name='logcollect',
-    version='0.9',
+    version='0.9.0',
+    long_description=read_md('README.md'),
     packages=['logcollect'],
     url='http://github.com/rutube/logcollect/',
     license='Beerware',
     author='tumbler',
     author_email='zimbler@gmail.com',
     description='Helper for collecting logs to ELK stack via RabbitMQ',
-    setup_requires=['amqp']
+    setup_requires=['amqp'],
 )
