e5203fc1b242d0895f0c6eef762d7bbbced5cc99
tumb1er <zimbler@gmail.com>
2015-08-19 14:54:32 +0300

    Version 0.10.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='logcollect',
-    version='0.9.1',
+    version='0.10.0',
     long_description=read_md('README.md'),
     packages=['logcollect'],
     url='http://github.com/rutube/logcollect/',
