a2322fdebf4e76c3b80605063b47e255b9363cd4
tumb1er <zimbler@gmail.com>
2015-10-23 15:24:04 +0300

    Bump version to 0.12.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='logcollect',
-    version='0.12.0',
+    version='0.12.1',
     long_description=read_md('README.md'),
     packages=['logcollect'],
     url='http://github.com/rutube/logcollect/',
